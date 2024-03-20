889d7cdaf1482a3f1ec4f9e7e6ebc4a82ebe74e4
Spencer Pearson <speeze.pearson@gmail.com>
2024-03-20 16:49:10 -0700

    lint

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export function Root() {
     return <>
-    <header>
-        {/* header stuff */}
-    </header>
-    <main>
-        <Outlet />
-    </main>
-    <footer>
-        {/* footer stuff */}
-    </footer>
+        <header>
+            {/* header stuff */}
+        </header>
+        <main>
+            <Outlet />
+        </main>
+        <footer>
+            {/* footer stuff */}
+        </footer>
     </>
 }
